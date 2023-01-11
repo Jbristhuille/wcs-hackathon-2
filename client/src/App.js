28846d2bb14cd0d8ef1b36c99a67e053d223c6f3
Jbristhuille <jbristhuille@gmail.com>
2023-01-11 16:12:14 +0100

    :sparkles: Setup parks map page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.scss';
+
 import QRCodePage from "./pages/QRCodePage/QRCodePage";
+import ParksMapPage from "./pages/ParksMapPage/ParksMapPage";
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/qr" element={<QRCodePage />}/>
+          <Route path="/parks" element={<ParksMapPage />} />
         </Routes>
       </Router>
     </div>
