49e6d69b067df9b57782b5fb7e87be1e78d004e1
Tarente Lorian <lorian.tarente@gmail.com>
2023-01-12 10:03:07 +0100

    generateur QRCode en back

--- server/controllers/qr.controller.js
+++ server/controllers/qr.controller.js
@@ -1,5 +1,9 @@
+const shajs = require("sha.js")
+
 const genQR = (req, res) => {
-    res.status(200).send("123");
+    
+    let hash = shajs("sha256").update(JSON.stringify(req.body)+String(Date.now())).digest('hex');
+    res.status(200).send(hash.substring(0, 10));
 }
 
 module.exports={
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json())
 
 const carRoute = require("./routes/car.route");
 const parkRoute = require("./routes/park.route");
